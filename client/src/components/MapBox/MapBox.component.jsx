fead453c3107c950b7d7c42b1b09c98216697e68
Gurkiran Singh <gurkiransinghk@gmail.com>
2021-02-02 20:10:43 -0500

    MODIFIED mapbox styles

--- client/src/components/MapBox/MapBox.component.jsx
+++ client/src/components/MapBox/MapBox.component.jsx
@@ -142,7 +142,6 @@ function MapBox(props) {
         mapStyle="mapbox://styles/gurkiransinghk/ckko751ud0rar17n5418ugu9y" 
         mapboxApiAccessToken={process.env.NODE_ENV === "development" ? "pk.eyJ1IjoiZ3Vya2lyYW5zaW5naGsiLCJhIjoiY2trbjJraXczMDVmYjJvcDU1bDRhMThjeCJ9._2ac1Xjtc_0ahJDnxHtU9A" : process.env.REACT_APP_MAPBOX_TOKEN}
         interactiveLayerIds={[clusterLayer.id]}
-        // onHover={handleMapHover}
         ref={mapRef}
         onClick={handleMapClick}
         >
