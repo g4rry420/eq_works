2c9bdc31c3c58cdab83d1c99c9e114f6b3291cda
gurkiran_singh <gurkiransinghk@gmail.com>
2021-03-29 18:30:19 -0400

    Added a loading message

--- client/src/components/spinner/spinner.component.jsx
+++ client/src/components/spinner/spinner.component.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React,{ Fragment } from 'react'
 import { motion } from "framer-motion"
 
 import "./spinner.styles.css"
 
 export default function Spinner() {
     return (
-        <motion.div className="spinner-overlay"
-        >
-            <div className="spinner-container">
-            </div>
-        </motion.div>
+        <Fragment>
+            <motion.div className="spinner-overlay"
+            >
+                <div className="spinner-container">
+                </div>
+            </motion.div>
+            <p>The backend is hosted in Glitch.So, it might take a minute to load. Please, wait. Thank You</p>
+        </Fragment>
     )
 }
